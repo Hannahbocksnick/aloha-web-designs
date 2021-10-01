209ddffc3e139b203b46535d465d7cb3695b9158
Hannahbocksnick <85531312+Hannahbocksnick@users.noreply.github.com>
2021-10-01 16:32:20 -0700

    jekyll install

---
+++ _site/scripts.js
@@ -0,0 +1,16 @@
+
+
+
+$('.hamburger-button').click(function(){
+    $('.mobile-menu').fadeToggle(1000);
+    $(this).toggleClass('active');
+});
+
+
+// Typing Animation
+var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings'
+});
+
+// Animate on scroll
+AOS.init();
