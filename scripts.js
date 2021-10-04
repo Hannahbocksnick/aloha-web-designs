cd2c9c9797abe9eeaa736335a1396a3f3de280a0
Hannahbocksnick <85531312+Hannahbocksnick@users.noreply.github.com>
2021-10-04 13:52:59 -0700

    adding animation and hamburger menu

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$('document').ready(function(){
+    
+
+     $('.hamburger-button').click(function(){
+        $(this).toggleClass('active');
+        $('.mobile-menu').fadeToggle(1000);
+    });
+
+    // Animate on Scroll - Activate
+
+    AOS.init();
+
+
+});
