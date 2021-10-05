f0d7b2cc08ad92176390204f4244e648bdd5681a
Hannahbocksnick <85531312+Hannahbocksnick@users.noreply.github.com>
2021-10-05 10:21:06 -0700

    typed

--- scripts.js
+++ scripts.js
@@ -6,18 +6,6 @@ $('document').ready(function(){
         $('.mobile-menu').fadeToggle(1000);
     });
 
-    // Typed.js Code
-    var typed = new Typed('#typed', {
-        stringsElement: '#typed-strings',
-        typeSpeed: 10,
-  	    startDelay: 500,
-  	    backSpeed: 10,
-  	    backDelay: 1000,
-  	    smartBackspace: true,
-  	    loop: true,
-  	    loopCount: Infinity
-    });
-
 
     // Animate on Scroll - Activate
 
