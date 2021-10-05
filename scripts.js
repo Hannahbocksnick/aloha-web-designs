f4a082c5d84ae87a1f83c7ad2a227abcd9107014
Hannahbocksnick <85531312+Hannahbocksnick@users.noreply.github.com>
2021-10-04 19:02:41 -0700

    typed.js

--- scripts.js
+++ scripts.js
@@ -6,9 +6,23 @@ $('document').ready(function(){
         $('.mobile-menu').fadeToggle(1000);
     });
 
+    // Typed.js Code
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 10,
+  	    startDelay: 500,
+  	    backSpeed: 10,
+  	    backDelay: 1000,
+  	    smartBackspace: true,
+  	    loop: true,
+  	    loopCount: Infinity
+    });
+
+
     // Animate on Scroll - Activate
 
     AOS.init();
 
+    
 
 });
